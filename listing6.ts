e1441c2b0b456d16ee1329d527179598ae74c39c
Rainald Menge-Sonnentag <rme@ix.de>
2022-07-29 15:39:43 +0200

    Delete listing6.ts

--- listing6.ts
+++
@@ -1,6 +0,0 @@
-function greet(person: string) {
-  return "Hello, " + person.toUpperCase()
-}
-
-greet("Susi"); // OK
-greet(null); // Fehler: Argument of type 'null' is not assignable to parameter of type 'string'
