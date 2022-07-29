7cdf796445ee0cac0d1d227e5fba150f25222755
Rainald Menge-Sonnentag <rme@ix.de>
2022-07-29 15:39:18 +0200

    Delete listing2.js

--- listing2.js
+++
@@ -1,8 +0,0 @@
-function sayHello(person) {
-  return "Hello, " + person.toUpperCase(); 
-} 
-
-sayHello("Klaus"); // OK
-sayHello(7); // Fehler in sayHello: 
-             // TypeError: person.toUpperCase 
-             // is not a function
